c99c0a7795aa63a2ec1d7e7984ac13ceae7cdec2
alexey.kireev <alexey.kireev@jetbrains.com>
2016-04-28 04:02:19 +0300

    updated onload and added onresize functions

--- src/fixer.js
+++ src/fixer.js
@@ -20,17 +20,10 @@ class Fixer {
     };
 
     // listen to the page load and scroll
-    window.onload = window.onscroll = function() {
-      this.listenScroll(window.pageYOffset || document.documentElement.scrollTop);
-    }.bind(this);
-  }
+    window.onload = window.onscroll = () => this.listenScroll(window.pageYOffset || document.documentElement.scrollTop);
 
-  /**
-   * Function listening scroll.
-   * @param {number} scrolled
-   */
-  listenScroll (scrolled) {
-    console.log(scrolled);
+    // listen to the page resize and recalculate elements width
+    window.onresize = () => this.recalculateElementsWidth(window.pageYOffset || document.documentElement.scrollTop);
   }
 
   /**
@@ -39,16 +32,16 @@ class Fixer {
    * @return {Element|boolean}
    */
   addElement (options) {
-    var element = null;
+    let element = null;
 
     if (options) {
       element = new Element(options);
-      
-      if (element.element.tagName) {
+
+      if (element.element && element.element.tagName) {
         this.elements.push(element);
       }
       else {
-        throw new Error("Can't add element, please check options", options);
+        throw new Error("Can't add element '" + options.element +"', please check the options", options);
       }
     }
     else {
@@ -57,6 +50,39 @@ class Fixer {
 
     return element;
   }
+
+  /**
+   * Function listening scroll.
+   * @param {number} scrolled Document scrolled height in pixels
+   */
+  listenScroll (scrolled) {
+    console.log(scrolled);
+  }
+
+  /**
+   * Recalculating width of the fixed elements (on resize).
+   * @param {number} scrolled Document scrolled height in pixels
+   */
+  recalculateElementsWidth (scrolled) {
+    let i = this.elements.length;
+    let item;
+
+    while (i--) {
+      item = this.elements[i];
+
+      if (
+        item.fixed &&                 // if element is fixed
+        item.placeholder &&           // if element have placeholder
+        item.placeholder.offsetWidth  // if placeholder have width value
+      ) {
+        // set the value of an element width equal to the width its placeholder
+        item.element.style.width = item.placeholder.offsetWidth + 'px';
+      }
+    }
+
+    // call listenScroll method to check all elements position
+    this.listenScroll(scrolled);
+  };
 }
 
 export default Fixer;
