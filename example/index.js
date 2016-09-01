e95d9b03d4e20c0f8ee5316aeee45d24570d823c
alexey.kireev <alexey.kireev@jetbrains.com>
2016-09-01 14:07:10 +0300

    Improve complexity of the example.

--- example/index.js
+++ example/index.js
@@ -1,19 +1,24 @@
-import Fixer from '../src/fixer';
-import './index.scss';
+import Fixer from "../src/fixer";
+import "./index.scss";
 
-let fixer = new Fixer();
+document.addEventListener("DOMContentLoaded", function () {
+  let fixer = new Fixer();
 
-fixer
-  .addElement('.menu')
-  .addElement('#side-block-1', {
-    limiter: '#side-block-2'
-  })
-  .addElement('#side-block-2', {
-    limiter: '.bottom-block'
-  })
-  .addElement('#bottom-block-1', {
-    position: 'bottom'
-  })
-  .addElement('#bottom-block-2', {
-    position: 'bottom'
-  });
+  fixer
+    .addElement(".menu", {
+      limit: "#side-block-1"
+    })
+    .addElement("#side-block-1", {
+      limit: "#side-block-2"
+    })
+    .addElement("#side-block-2", {
+      limit: ".bottom-block"
+    })
+    .addElement("#bottom-block-1", {
+      position: "bottom"
+    })
+    .addElement("#bottom-block-2", {
+      position: "bottom",
+      limit: "#bottom-block-1"
+    });
+}, false);
