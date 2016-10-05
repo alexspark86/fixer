660648bb09734db8f06f03075f42a30edc7bc4c0
alexey.kireev <alexey.kireev@jetbrains.com>
2016-10-05 19:07:12 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,30 @@
+import Fixer from "../src/fixer";
+import WebFont from "webfontloader";
+import "./index.scss";
+
+WebFont.load({
+  google: {
+    families: ["Merriweather", "PT Serif"]
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  let fixer = new Fixer();
+
+  fixer
+    .addElement(".menu", {
+      setWidth: false
+    })
+    .addElement("#side-block-1", {
+      limit: "#side-block-2"
+    })
+    .addElement("#side-block-2", {
+      limit: ".bottom-block"
+    })
+    .addElement("#bottom-block-1", {
+      position: "bottom"
+    })
+    .addElement("#bottom-block-2", {
+      position: "bottom"
+    });
+}, false);
